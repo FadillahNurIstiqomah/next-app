0ab97048360a433055c6d60493bcd80c8c97f0aa
FadillahNurIstiqomah <fadillahistiqomah@students.amikom.ac.id>
2024-02-03 23:20:50 +0700

    Fixing Bug

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -6,12 +6,15 @@ import { useMutation } from "react-query";
 import { logout } from "./api/auth";
 import Cookies from 'js-cookie';
 import Swal from "sweetalert2";
+import { useRouter } from "next/router";
 
 
 export default function Main() {
+  const router = useRouter();
   const name = Cookies.get('user name');
+  const token = Cookies.get('authToken')
 
-const mutation = useMutation(logout, {
+  const mutation = useMutation(logout, {
     onSuccess: () => {
       Swal.fire({
         icon: 'success',
@@ -20,41 +23,75 @@ const mutation = useMutation(logout, {
       });
     }
   });
-
+  
+  // if (!token) {
+  //   // router.push('/login');
+  // }
   const handleLogout = () => {
     mutation.mutate();
   };
 
   return (
     <div>
-      <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-        <div className="absolute -z-10 w-full min-h-screen">
-          <Image
-            src={bg}
-            alt="background"
-            className="opacity-70 lg:scale-[2.0] scale-[3.0]"
-          />
-        </div>
-        <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
-          <h1 className="text-3xl font-bold text-center text-gray-700 my-3">Hello, {name} kamu berhasil login!</h1>
-            <Link
-              href="/login"
-            >
-              <button
-                disabled={mutation.isLoading}
-                onClick={handleLogout} 
-                className="w-full px-4 py-2 my-4 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
-              >
-                {mutation.isLoading ? 'Logging out...' : 'Logout'}
-              </button>
-            </Link>
-            <Image 
-              src={logo}
-              alt="logo"
-              className="w-[160px] h-[80px] lg:ml-[180px] ml-[60px] mt-3"
+      {token ? (
+        <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+          <div className="absolute -z-10 w-full min-h-screen">
+            <Image
+              src={bg}
+              alt="background"
+              className="opacity-70 lg:scale-[2.0] scale-[3.0]"
             />
+          </div>
+          <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+            <h1 className="text-3xl font-bold text-center text-gray-700 my-3">Hello, {name} kamu berhasil login!</h1>
+              <Link
+                href="/login"
+              >
+                <button
+                  disabled={mutation.isLoading}
+                  onClick={handleLogout} 
+                  className="w-full px-4 py-2 my-4 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+                >
+                  {mutation.isLoading ? 'Logging out...' : 'Logout'}
+                </button>
+              </Link>
+              <Image 
+                src={logo}
+                alt="logo"
+                className="w-[160px] h-[80px] lg:ml-[180px] ml-[60px] mt-3"
+              />
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+            <div className="absolute -z-10 w-full min-h-screen">
+              <Image
+                src={bg}
+                alt="background"
+                className="opacity-70 lg:scale-[2.0] scale-[3.0]"
+              />
+            </div>
+            <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+              <h1 className="text-3xl font-bold text-center text-gray-700 my-3">Restricted Page, Please Login</h1>
+                <Link
+                  href="/login"
+                >
+                  <button
+                    className="w-full px-4 py-2 my-4 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+                  >
+                    Login
+                  </button>
+                </Link>
+                <Image 
+                  src={logo}
+                  alt="logo"
+                  className="w-[160px] h-[80px] lg:ml-[180px] ml-[60px] mt-3"
+                />
+            </div>
+          </div>
+        </div>
+      )}
   </div>
   );
 }
